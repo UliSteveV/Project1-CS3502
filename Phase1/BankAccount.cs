2c07fb7e0dcbe7293eff622bb0c1164012b53524
UliSteveV <ulystevev@gmail.com>
2025-02-11 20:44:57 -0500

    Added basic methods to the BankAccount class

--- Phase1/BankAccount.cs
+++ Phase1/BankAccount.cs
@@ -1,4 +1,29 @@
 class BankAccount{
-    
+    private object balancelock = new object();
+    public int Balance {get; private set;}
+
+    public BankAccount(int initialBalance){
+        Balance = initialBalance;
+    }
+
+    public void Deposit(int amount){
+        lock(balancelock){
+            Balance += amount;
+            Console.WriteLine($"Deposited {amount}, balance is now {Balance}");
+        }
+    }
+
+    public void withdraw(int amount){
+        lock(balancelock){
+            if(Balance >= amount){
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn {amount}, balance is now {Balance}");
+            }
+            else{
+                Console.WriteLine($"Sorry, not enough funds to withdraw {amount}");
+            }
+            
+        }
+    }
 
 }
